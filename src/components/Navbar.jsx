b73e25dc63f0bdf8eccefc1de7d32f2c3ac98030
Nilesh Kumar <nileshkumar12521@gmail.com>
2026-07-17 08:14:39 +0530

    Project completed ... I made the Navbar1 all link (BUtton) more functioal , like when user will click it will change the olor

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -6,11 +6,11 @@ const Navbar = () => {
     <div className='flex px-4 py-4 bg-cyan-800 items-center justify-between'>
         <h2 className='text-xl font-bold '>Shariyens </h2>
         <div className='flex gap-10'>
-            <Link className='text-lg font-medium' to='/' >Home</Link>
-            <Link className='text-lg font-medium' to='/About' >About</Link>
-            <Link className='text-lg font-medium' to='/Contact' >Contact</Link>
-            <Link className='text-lg font-medium' to='/Product' >Product</Link>
-            <Link className='font-medium text-lg ' to='/Courses'>Courses</Link>
+            <Link className='text-lg font-medium cursor-pointer active:text-emerald-500' to='/' >Home</Link>
+            <Link className='text-lg font-medium active:text-emerald-500' to='/About ' >About</Link>
+            <Link className='text-lg font-medium active:text-emerald-500' to='/Contact' >Contact</Link>
+            <Link className='text-lg font-medium active:text-emerald-500' to='/Product' >Product</Link>
+            <Link className='font-medium text-lg active:text-emerald-500 ' to='/Courses'>Courses</Link>
         </div>
     </div>
   )
--- src/components/Navbar2.jsx
+++ src/components/Navbar2.jsx
@@ -7,11 +7,7 @@ const NavBar2 = () => {
     let navigate = useNavigate()
 
   return (
-<>
 
-    <div className="bg-red-500 text-white p-10">
-  Test
-</div>
     <div className='px-5 py-3 bg-cyan-400'>
         <button onClick={()=>{
         navigate('/')
@@ -20,8 +16,11 @@ const NavBar2 = () => {
       <button onClick={()=>{
         navigate(-1)  // -1 is use to go the page back.   
       }} className='bg-amber-500 px-5 py-2 rounded m-2 cursor-pointer active:scale-95' >Back</button>
+
+      <button onClick={()=>{
+        navigate(+1)  // -1 is use to go the page back.   
+      }} className='bg-amber-500 px-5 py-2 rounded m-2 cursor-pointer active:scale-95' >Next</button>
     </div>
-    </>
   )
 }
 
